021e47b2adddb87f98debc6c1395ee86e3ec651b
PabloBogg <pablo_boggetti@hotmail.com>
2025-04-03 20:52:23 -0300

    unidad 11 - modulo 4

---
+++ UNIDAD 11 - PERFORMANCE/MODULO 4 - Integración de JMeter con JAVA/jmeterproyect/src/test/java/com/jmeterproyect/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jmeterproyect;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
