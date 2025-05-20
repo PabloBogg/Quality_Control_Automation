e4f942cf58c5d7e066cf928cf3651d9e28246d9b
Pablo Boggetti <120680598+PabloBogg@users.noreply.github.com>
2025-05-19 21:00:25 -0300

    Delete MODULO 8 - Clase Math directory

--- MODULO 8 - Clase Math/actividadGenerandoNumerosAleatorios.java
+++
@@ -1,9 +0,0 @@
-/* ✏️  Actividad: Generando números aleatorios
-Escribe un programa que genere y muestre en pantalla un número
- aleatorio en el rango del 1 al 355  utilizando el método random()
-  de la clase Math. Puedes utilizar el método floor() para redondear
-   el número aleatorio hacia abajo. */
-
-public class actividadGenerandoNumerosAleatorios {
-    
-}
--- MODULO 8 - Clase Math/redondeoNumeros.java
+++
@@ -1,8 +0,0 @@
-/* ✏️  Actividad: Redondeo de números
-Escribe un programa que pida al usuario un número decimal
- y muestra en pantalla su valor redondeado utilizando el 
- método round() de la clase Math. */
-
-public class redondeoNumeros {
-    
-}
